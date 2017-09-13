ffff015a749ee85f76f983e8dcae526e1fe49f73
ivobar <babaginka@students.softuni.bg>
2017-09-13 23:31:38 +0300

    Venues list displays number of events for each venue.

--- js/handlers/venuesHandlers.js
+++ js/handlers/venuesHandlers.js
@@ -6,21 +6,31 @@ handlers.venuesList = function (ctx) {
     }
 
     venueService.getVenues(authorization)
-        .then(function (data) {
-            data.sort(venueService.compareVenues);
-            ctx.venues = data;
-            for (let venue of ctx.venues) {
-                venue.isAdmin = ctx.isAdmin;
-            }
+        .then(function (venues) {
+            venues.sort(venueService.compareVenues);
+            ctx.venues = venues;
+            eventService.getAllEvents(authorization)
+                .then(function (events) {
+                    for (let venue of ctx.venues) {
+                        venue.events=0;
+                        venue.isAdmin = ctx.isAdmin;
+                        for (let event of events) {
+                            if(event.venue===venue._id){
+                                venue.events++;
+                            }
+                        }
+                    }
 
-            ctx.loadPartials({
-                header: './templates/common/header.hbs',
-                footer: './templates/common/footer.hbs',
-                venue: './templates/venuesList/venue.hbs'
-            }).then(function () {
-                ctx.partials = this.partials;
-                ctx.partial('./templates/venuesList/venuesList.hbs');
-            })
+                    ctx.loadPartials({
+                        header: './templates/common/header.hbs',
+                        footer: './templates/common/footer.hbs',
+                        venue: './templates/venuesList/venue.hbs'
+                    }).then(function () {
+                        ctx.partials = this.partials;
+                        ctx.partial('./templates/venuesList/venuesList.hbs');
+                    })
+                })
+
         })
 };
 
